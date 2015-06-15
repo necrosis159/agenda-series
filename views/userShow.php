e6848ab91b170adbe2644ef05071baa8aa1bc48b
thekriss77 <christophe@lugand.fr>
2015-06-15 21:34:43 +0200

    Gestion user innexistant

--- views/userShow.php
+++ views/userShow.php
@@ -1,6 +1,11 @@
 <?php 
 //Affichage de l'objet user qui est un tableau (même avec un seul élément)
-
-echo $user[0]->name;
-echo $user[0]->surname;
-?>
+if(!empty($user))
+{
+	echo $user[0]->name;
+	echo $user[0]->surname;
+}
+else
+{
+	echo 'Aucun utilisateur trouve';
+}
